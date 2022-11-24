848185dc0e6e755fd5996c8a5e5772821abe3feb
aselka <bis-aselya@yandex.ru>
2022-11-24 14:46:37 +0400

    Update conftest.py
    полноэкранный вид после загрузки браузера

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selene.support.shared import browser
 
 
-
 @pytest.fixture(scope='function', autouse=True)
 def screen_config():
-    browser.open('https://google.com').driver.maximize_window()
+    browser.open('https://google.com')
+    browser.driver.maximize_window()
