8abfa524d16948905fa36189782b76e147d1a9a5
Asel <bis-aselya@yandex.ru>
2022-12-20 23:45:02 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,5 +4,6 @@ from selene.support.shared import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def screen_config():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     browser.open('https://google.com')
-    browser.driver.maximize_window()
